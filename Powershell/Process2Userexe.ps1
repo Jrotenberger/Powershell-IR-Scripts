eda135868046325672962e1a5f3ed6f33cca1b46
Jrotenberger <Jrotenberger@users.noreply.github.com>
2019-05-09 13:53:29 -0500

    Delete Process2Userexe.ps1
    Not needed in new script

--- Powershell/Process2Userexe.ps1
+++
@@ -1,8 +0,0 @@
-
-
-
-
-gwmi win32_process |ft Name, ProcessID, ParentProcessID -a > C:\ProcessInfo_2_process-to.exe-mapping.txt
-
-
-(Get-WmiObject -class win32_process | where{$_.ProcessName -eq 'mshta.exe'}).getowner() | Select -property domain, user 

e507d969f9ad206aeba0326b12aaf35cb677d87a
Jrotenberger <Jrotenberger@users.noreply.github.com>
2019-05-09 13:55:23 -0500

    Delete childprocess.ps1
    Not needed in new script

--- Powershell/childprocess.ps1
+++
@@ -1,9 +0,0 @@
-
-
-$processcollection = Get-WMIObject Win32_process | Select-Object Name, Description, ProcessID, ParentProcessID, MachineName, PriorityClass, ProcessName, SessionID, Path 
-
-foreach ($process in $processcollection)
-	{
-	Write-Host ($_.name + ' ' + $_.ProcessID + ' ' + $_.ParentProcessID)
-	Group-Object -property ParentProcessID 
-		}

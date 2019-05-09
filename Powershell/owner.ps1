52452bf49a8c96abfd37d1f9d6eac769d1fb9af5
Jrotenberger <Jrotenberger@users.noreply.github.com>
2019-05-09 13:56:45 -0500

    Delete owner.ps1
    Not needed in new script

--- Powershell/owner.ps1
+++
@@ -1,4 +0,0 @@
-. \powershell\Get-ProcessOwner
-
-get-wmiobject win32_process | Get-ProcessOwner | Select Name,CreationDate,Priority,ProcessID,ParentProcessID,Path,Owner,Computername |Out-File C:\ProcessInfo_3_process-to-user-mapping.txt
-get-wmiobject win32_process | Get-ProcessOwner | Select Name,CreationDate,Priority,ProcessID,ParentProcessID,Path,Owner,Computername |Export-CSV c:\ProcessInfo_3_process-to-user-mapping.csv
